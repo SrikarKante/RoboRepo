976d4e5e38a14e37c21e2689d679c08122d92cf3
Slarrty <slarty@bardfarst.com>
2016-05-26 19:41:53 -0400

    Added buttons and manip controller to OI

--- PIDControlDrive/src/org/usfirst/frc/team226/robot/OI.java
+++ PIDControlDrive/src/org/usfirst/frc/team226/robot/OI.java
@@ -1,6 +1,11 @@
 package org.usfirst.frc.team226.robot;
 
+import org.usfirst.frc.team226.robot.commands.SetFourBarPoint;
+import org.usfirst.frc.team226.robot.subsystems.FourBar;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -9,11 +14,28 @@ import edu.wpi.first.wpilibj.Joystick;
 public class OI {
 	
 	Joystick driver = new Joystick(0);
+	Joystick manip = new Joystick(1);
 	
-	Button A = new JoystickButton(driver, ****);
+	Button D_A = new JoystickButton(driver, 1);
+	Button D_X= new JoystickButton(driver, 3);
+	Button D_LB = new JoystickButton(driver, 5);
+	Button D_RB = new JoystickButton(driver, 6);
+	Button D_LS = new JoystickButton(driver, 9);
+	Button M_A = new JoystickButton(manip, 1);
+	Button M_B = new JoystickButton(manip, 2);
+	Button M_X = new JoystickButton(manip, 3);
+	Button M_Y = new JoystickButton(manip, 4);
+	Button M_LB = new JoystickButton(manip, 5);
+	Button M_RB = new JoystickButton(manip, 6);
+	Button M_SELECT = new JoystickButton(manip, 7);
+	Button M_START = new JoystickButton(manip, 8);
+	Button M_LS = new JoystickButton(manip, 9);
+	Button M_RS = new JoystickButton(manip, 10);
 	
 	public OI() {
-		
+		M_A.whenPressed(new SetFourBarPoint(FourBar.ZERO));
+		M_X.whenPressed(new SetFourBarPoint(FourBar.HALF));
+		M_Y.whenPressed(new SetFourBarPoint(FourBar.FULL));
 	}
 	
 	public double getLeftJoystick() {
