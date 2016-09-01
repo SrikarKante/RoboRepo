36ea87d6ceb7c8b5c7a5644b73a883c8783aa74e
Slarrty <slarty@bardfarst.com>
2016-09-01 11:00:20 -0400

    Updated XboxController, added demo drive code

--- ChassisBotV2/src/org/usfirst/frc/team226/robot/OI.java
+++ ChassisBotV2/src/org/usfirst/frc/team226/robot/OI.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team226.robot;
 
+import org.usfirst.frc.team226.robot.commands.DemoDriveToggle;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -7,8 +9,10 @@ package org.usfirst.frc.team226.robot;
 public class OI {
 	
 	public XboxController driveController = new XboxController(0);
+	public XboxController demoController = new XboxController(1);
 	
 	public void OI() {
+		driveController.A.whenPressed(new DemoDriveToggle());
 	}
 }
 
--- ChassisBotV2/src/org/usfirst/frc/team226/robot/commands/XboxArcadeDrive.java
+++ ChassisBotV2/src/org/usfirst/frc/team226/robot/commands/XboxArcadeDrive.java
@@ -9,35 +9,47 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class XboxArcadeDrive extends Command {
 
-    public XboxArcadeDrive() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.driveTrain);
-    }
+	public static boolean demo = false;
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	public XboxArcadeDrive() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.driveTrain);
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	double throttle = Robot.oi.driveController.getLeftJoystick_Y();
-    	double turn = Robot.oi.driveController.getRightJoystick_X();
-    	
-    	Robot.driveTrain.arcadeDrive(throttle, turn, true);
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		if (!demo) {
+			double throttle = Robot.oi.driveController.getLeftJoystick_Y();
+			double turn = Robot.oi.driveController.getRightJoystick_X();
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+			Robot.driveTrain.arcadeDrive(throttle, turn, true);
+		}
+		else if (demo) {
+			if (Robot.oi.driveController.getLeftJoystick_Y() > ) {
+			double throttle = (0.5*Robot.oi.demoController.getLeftJoystick_Y());
+			}
+			double turn = (0.5*Robot.oi.demoController.getRightJoystick_X());
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+			Robot.driveTrain.arcadeDrive(throttle, turn, true);
+		}
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
