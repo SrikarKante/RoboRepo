9b684ca1a76d0edbf03dde9e2c94fc512c2ce8be
Slarrty <slarty@bardfarst.com>
2016-09-01 12:00:50 -0400

    Revised demo drive code

--- ChassisBotV2/src/org/usfirst/frc/team226/robot/commands/XboxArcadeDrive.java
+++ ChassisBotV2/src/org/usfirst/frc/team226/robot/commands/XboxArcadeDrive.java
@@ -28,13 +28,17 @@ public class XboxArcadeDrive extends Command {
 			double turn = Robot.oi.driveController.getRightJoystick_X();
 
 			Robot.driveTrain.arcadeDrive(throttle, turn, true);
-		}
-		else if (demo) {
-			if (Robot.oi.driveController.getLeftJoystick_Y() > ) {
-			double throttle = (0.5*Robot.oi.demoController.getLeftJoystick_Y());
+		} else if (demo) {
+			double throttle = 0;
+			double turn = 0;
+			if (Robot.oi.driveController.getLeftJoystick_Y() > 0) {
+				throttle = Robot.oi.driveController.getLeftJoystick_Y();
+			} else if (Robot.oi.driveController.getRightJoystick_X() > 0) {
+				turn = Robot.oi.driveController.getRightJoystick_X();
+			} else {
+				throttle = (0.5 * Robot.oi.demoController.getLeftJoystick_Y());
+				turn = (0.5 * Robot.oi.demoController.getRightJoystick_X());
 			}
-			double turn = (0.5*Robot.oi.demoController.getRightJoystick_X());
-
 			Robot.driveTrain.arcadeDrive(throttle, turn, true);
 		}
 	}
